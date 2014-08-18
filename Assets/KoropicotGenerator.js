fb35627bd301d5dabacc3084075088bb2a67adb3
mzp <mzpppp@gmail.com>
2014-08-19 07:37:59 +0900

    add force

--- Assets/KoropicotGenerator.js
+++ Assets/KoropicotGenerator.js
@@ -1,13 +1,20 @@
 #pragma strict
 
-var prefab : GameObject;
+var target : Rigidbody;
+var basePos : Vector3 = new Vector3(0,0,0);
 
 function Start () {
 
 }
 
-function Update () {
+function FixedUpdate () {
   if (Input.GetMouseButtonDown(0)) {
-    Instantiate(prefab, Vector3(Random.Range(-0.1,0.1), 3, Random.Range(-0.1,0.1)), Quaternion.identity);
+     this.basePos = Input.mousePosition;
+  }
+  if (Input.GetMouseButton(0)) {
+    var y : float = Input.mousePosition.y - basePos.y;
+    var x : float = Input.mousePosition.x - basePos.x;
+
+    target.AddForce(new Vector3(y/100, 0, x/100));
   }
 }
