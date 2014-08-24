78b769983c3b0406c327f6e5e8d43947a987c9ce
mzp <mzpppp@gmail.com>
2014-08-24 14:16:53 +0900

    use gyro sensor at iPhone

--- Assets/KoropicotGenerator.js
+++ Assets/KoropicotGenerator.js
@@ -3,7 +3,8 @@
 var target  : Rigidbody;
 var basePos : Vector3 = new Vector3(0,0,0);
 var power   : float = 0.01;
- 
+var accelerationPower : float = - 0.0001;
+
 function FixedUpdate () {
   if (Input.GetMouseButtonDown(0)) {
      this.basePos = Input.mousePosition;
@@ -13,5 +14,13 @@ function FixedUpdate () {
     var x : float = Input.mousePosition.x - basePos.x;
 
     target.AddForce(new Vector3(x * power, 0, y * power), ForceMode.Impulse);
+  } else {
+
+#if UNITY_IPHONE
+    Debug.Log(Input.gyro.gravity * accelerationPower);
+    target.AddForce(new Vector3(Input.gyro.gravity.x * accelerationPower,
+                                0,
+                                Input.gyro.gravity.y * accelerationPower), ForceMode.Impulse);
+#endif
   }
 }
