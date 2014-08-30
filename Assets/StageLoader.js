8286259545c9a4b062556290e2d3b5f91473ff6e
mzp <mzpppp@gmail.com>
2014-08-31 08:19:11 +0900

    more complex stage

--- Assets/StageLoader.js
+++ Assets/StageLoader.js
@@ -4,6 +4,7 @@ var stageDefinition : TextAsset;
 var block  : GameObject;
 var player : GameObject;
 var parent : Transform;
+var goal   : GameObject;
 
 function Start () {
   var x : Number = 0;
@@ -12,14 +13,19 @@ function Start () {
   for(line in stageDefinition.text.Split('\n'[0])) {
     for(cell in line) {
       if(cell == "*") {
-        var gameObject : GameObject = Instantiate(block);
-        gameObject.transform.position.x = x;
-        gameObject.transform.position.y = 0.4;
-        gameObject.transform.position.z = y;
-        gameObject.transform.parent = parent;
+        var b : GameObject = Instantiate(block);
+        b.transform.position.x = x;
+        b.transform.position.y = 0.4;
+        b.transform.position.z = y;
+        b.transform.parent = parent;
       } else if (cell == "k") {
         player.transform.position.x = x;
         player.transform.position.z = y;
+      } else if(cell == "G") {
+        var g : GameObject = Instantiate(goal);
+        g.transform.position.x = x;
+        g.transform.position.z = y;
+        g.transform.parent = parent;
       }
       x += 1;
     }
